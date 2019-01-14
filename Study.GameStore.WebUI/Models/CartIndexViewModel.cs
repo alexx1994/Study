5da7a5d696ca81d3873349b7b63c3aa1e61972d4
Alex Franchuk <Alexx.fr.1994@gmail.com>
2019-01-14 17:19:05 +0200

    Finished five tuts by e commerce

---
+++ Study.GameStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Study.GameStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Study.GameStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
