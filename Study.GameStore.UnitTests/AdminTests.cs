ee88eacb29e8e5008db4d23f0ddf8166a2b624af
Alex Franchuk <Alexx.fr.1994@gmail.com>
2019-01-15 10:41:22 +0200

    Finished nine tuts by e commerce

---
+++ Study.GameStore.UnitTests/AdminTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Study.GameStore.Domain.Abstract;
+using Study.GameStore.Domain.Entities;
+using Study.GameStore.WebUI.Controllers;
+using System.Web.Mvc;
+
+namespace Study.GameStore.UnitTests
+{
+    [TestClass]
+    public class AdminTests
+    {
+        [TestMethod]
+        public void Index_Contains_All_Games()
+        {
+            // Организация - создание имитированного хранилища данных
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games).Returns(new List<Game>
+            {
+                new Game { GameId = 1, Name = "Игра1"},
+                new Game { GameId = 2, Name = "Игра2"},
+                new Game { GameId = 3, Name = "Игра3"},
+                new Game { GameId = 4, Name = "Игра4"},
+                new Game { GameId = 5, Name = "Игра5"}
+            });
+
+            // Организация - создание контроллера
+            AdminController controller = new AdminController(mock.Object);
+
+            // Действие
+            List<Game> result = ((IEnumerable<Game>)controller.Index().
+                ViewData.Model).ToList();
+
+            // Утверждение
+            Assert.AreEqual(result.Count(), 5);
+            Assert.AreEqual("Игра1", result[0].Name);
+            Assert.AreEqual("Игра2", result[1].Name);
+            Assert.AreEqual("Игра3", result[2].Name);
+        }
+    }
+}
